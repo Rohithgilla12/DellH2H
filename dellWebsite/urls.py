63c4e9cd4e6737b853beafe8591751e51cff239d
#Gilla_Dude! <gillarohith1@gmail.com>
2019-08-18 15:24:12 +0530

    Home page card

--- dellWebsite/urls.py
+++ dellWebsite/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from product.views import *
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     url('admin/', admin.site.urls),
@@ -28,4 +29,8 @@ urlpatterns = [
     url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),
     url(r'^login/$', auth_views.login, {'template_name': 'login.html'},name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout'),
-]
+    url(r'products/(?P<pk>\d{0,50})/$', productDetailView, name='productDetailView'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- product/views.py
+++ product/views.py
@@ -4,4 +4,16 @@ from .models import *
 # Create your views here.
 
 def home(request, *args, **kwargs):
-    return render(request, 'home.html', {})
+    qset = Product.objects.values()
+    context = {
+        "qset":qset
+    }
+    return render(request, 'home.html', context)
+
+def productDetailView(request, *args, **kwargs):
+    pk = kwargs['pk']
+    obj = Product.objects.get(sku = pk)
+    context = {
+        "object" : obj
+    }
+    return render(request, 'productDetail.html', context)
